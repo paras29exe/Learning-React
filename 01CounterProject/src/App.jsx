827c480ab46c88d857e7f4e62a8fffd7027579c5
Paras <dimplesaini5252@gmail.com>
2024-05-24 12:56:48 +0530

    Edit

--- 01CounterProject/src/App.jsx
+++ 01CounterProject/src/App.jsx
@@ -18,8 +18,14 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button>
           count is {count}
+        </button>
+        <button onClick={() => setCount(count + 1)}>
+          Increase count
+        </button>
+        <button onClick={() => setCount(count - 1)}>
+          Decrease count
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
