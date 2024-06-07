2a216d2f6a5bf4b640318c628132f3a989acf42f
paras29exe <dimplesaini5252@gmail.com>
2024-06-07 17:58:30 +0530

    learnt react redux

---
+++ ReactRedux/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import AddTodo from './components/Addtodo'
+import TodoList from './components/TodoList'
+
+function App() {
+
+  return (
+    <>
+    {/* addtodo component */}
+    <AddTodo />
+    {/* todolist component */}
+    <TodoList />
+    </>
+  )
+}
+
+export default App
